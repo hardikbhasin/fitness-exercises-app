44cae9296e6fea03b03c0c662fa6e52b98a8a395
hardikbhasin <hardikbhasin398@gmail.com>
2023-05-31 21:06:14 +0530

    added components(HeroBanner/Exercises/Search)

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,7 +5,7 @@ import Logo from '../assets/images/Logo.png';
 import { borderBottom } from '@mui/icons-material';
 const Navbar = () => {
   return (
-    <Stack direction="row"  justifyContent={'space-around'} sx={{gap : {sm:'122px', xs: '40px'}, mt: {sm: '32px', sx:'20px'}}}>
+    <Stack direction="row"  justifyContent={'space-around'} sx={{gap : {sm:'122px', xs: '40px'}, mt: {sm: '32px', sx:'20px'}, justifyContent:'none'}} px='20px'>
         <Link to ="/">
         <img src={Logo} alt="logo" style={{width : '48px', height: '48px', margin: '0 20px'}}/>
         </Link>
