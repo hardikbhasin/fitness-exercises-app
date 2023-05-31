d88dc1be0f6916cd8c23b80149dc694622548d02
hardikbhasin <hardikbhasin398@gmail.com>
2023-05-31 20:28:41 +0530

    basic folder and files

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import ReactDOM   from 'react-dom';
+import ReactDOM from 'react-dom';
 import App from './App'
+import { BrowserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+
 );
