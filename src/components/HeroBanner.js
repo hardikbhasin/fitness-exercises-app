39e62bf0860d61e7bde95c54ff0f9988bf53dfe8
hardikbhasin <hardikbhasin398@gmail.com>
2023-06-02 20:10:00 +0530

    started heroBanner

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,10 +1,22 @@
 import React from 'react'
-
+import { Box, Stack, Typography } from '@mui/material';
 const HeroBanner = () => {
   return (
-    <div>
-      HeroBanner
-    </div>
+    <Box sx={{
+      mt: { lg: '212px', xs: '70px' },
+      ml: { sm: '50px' }
+    }} position="relative" p="20px">
+      <Typography color="#FF2625" fontWeight="600" fontSize="26px" >
+        💪My Fitness Club
+      </Typography>
+      <Typography>
+          Sweat💦,Smile😊<br/>and Repeat🔁
+
+      </Typography>
+      <Typography>
+          Check out the most effectiev exercises here!
+      </Typography>
+    </Box>
   )
 }
 
