58d919b398070f627ce47415811bedbf6cda4928
hardikbhasin <hardikbhasin398@gmail.com>
2023-06-12 00:49:12 +0530

    edits in BodyPart as component

--- src/components/SearchExercises.js
+++ src/components/SearchExercises.js
@@ -2,9 +2,9 @@ import React, { useEffect, useState } from 'react'
 import { Box, Stack, Button, TextField, Typography } from '@mui/material';
 import { exerciseOptions, fetchData } from '../utils/fetchData';
 import HorizontalScrollbar from './HorizontalScrollbar';
-const SearchExercises = () => {
+const SearchExercises = ({setExercises, bodyPart, setBodyPart}) => {
   const [search, setSearch] = useState('')
-  const [exercises, setExercises] = useState([]);
+  
   const [bodyParts, setBodyParts] = useState([]);
   useEffect(() => {
     const fetchExercisesData = async () => {
@@ -67,7 +67,7 @@ const SearchExercises = () => {
         </Button>
       </Box>
       <Box sx={{position: 'relative', width : '100%', p: '20px'}}>
-        <HorizontalScrollbar data={bodyParts} />
+        <HorizontalScrollbar data={bodyParts} bodyPart={bodyPart} setbodyPart={setBodyPart}/>
 
       </Box>
     </Stack>
