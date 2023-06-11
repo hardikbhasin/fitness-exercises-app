fd989d265f3218886de652147bc740fd4e058264
hardikbhasin <hardikbhasin398@gmail.com>
2023-06-12 00:04:39 +0530

    fixed error in api

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -5,7 +5,7 @@ export const exerciseOptions = {
 
     headers: {
         'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY
+        'X-RapidAPI-Key': 'd8de15af0fmshd0fc820092f57adp181ecejsn28f8c61cf742'
         
     },
 };
