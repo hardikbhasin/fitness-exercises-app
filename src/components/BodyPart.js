12f80418fa619df2f03ad0763938ce6f9731d537
hardikbhasin <hardikbhasin398@gmail.com>
2023-06-13 00:03:44 +0530

    added content for cards

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,10 +1,28 @@
 import React from 'react'
-
-const BodyPart = () => {
+import { Stack, Typography } from '@mui/material'
+import Icon from '../assets/icons/gym.png'
+import { BorderBottom } from '@mui/icons-material'
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
   return (
-    <div>
-      BodyPart
-    </div>
+    <Stack
+      type='button'
+      alignItems='center'
+      justifyContent='center'
+      className='bodyPart-card'
+      sx={{
+      borderTop : bodyPart === item ? '4px solid #FF2625' : '',
+      backgroundColor: '#fff',
+      borderBottomLeftRadius: '20px',
+      width : '270px',
+      height: '280px',
+      cursor: 'pointer', gap: '47px'
+}}
+      
+
+    >
+  <img src={Icon} alt='dumbbell' style={{ width: '40px', height: '40px' }} />
+    </Stack >
+
   )
 }
 
