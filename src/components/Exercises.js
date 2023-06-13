43ba4b99f7cbfb27e0bfca3aa0312f4221a87afa
hardikbhasin <hardikbhasin398@gmail.com>
2023-06-14 00:31:13 +0530

    checked working of search

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -1,18 +1,32 @@
-import React, {useEffect, useState} from 'react'
+import React, { useEffect, useState } from 'react'
 import Pagination from '@mui/material/Pagination'
-import {Box, Stack, Typography} from '@mui/material'
+import { Box, Stack, Typography } from '@mui/material'
 import { exerciseOptions, fetchData } from '../utils/fetchData'
 
-const Exercises = ({exercises, setExercise, bodyPart}) => {
+const Exercises = ({ exercises, setExercise, bodyPart }) => {
+  console.log(exercises);
   return (
     <Box id="exercises"
-    sx={{mt : {lg : '110px'}}}
-    mt='50px'
-    p='20px'
+      sx={{ mt: { lg: '110px' } }}
+      mt='50px'
+      p='20px'
     >
-      <Typography>
+      <Typography variant='h3' mb='46px'>
         Showing Results
       </Typography>
+      <Stack direction='row' sx={{ gap: { lg: '110px', xs: '50px' } }}
+        flexWrap='wrap'
+        justifyContent='center'
+      >
+        {exercises.map((exercise, index) =>(
+          <p>{exercise.name}</p>
+
+
+        ))}
+
+
+
+      </Stack>
     </Box>
   )
 }
