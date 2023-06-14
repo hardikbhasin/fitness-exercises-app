a9a51940fa411a2bc9e2355baaa47da9639a919a
hardikbhasin <hardikbhasin398@gmail.com>
2023-06-15 00:30:59 +0530

    added components related to details

--- src/pages/ExerciseDetail.js
+++ src/pages/ExerciseDetail.js
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import { Box } from '@mui/material';
+import { exerciseOptions, fetchData, youtubeOptions } from '../utils/fetchData';
+import Detail from '../components/Detail';
+import ExerciseVideos from '../components/ExerciseVideos';
+import SimilarExercises from '../components/SimilarExercises';
 
 const ExerciseDetail = () => {
   return (
-    <div>
-      ExerciseDetail
-    </div>
-  )
+    <Box sx={{ mt: { lg: '96px', xs: '60px' } }}>
+      <Detail exerciseDetail={exerciseDetail} />
+      <ExerciseVideos exerciseVideos={exerciseVideos} name={exerciseDetail.name} />
+      <SimilarExercises targetMuscleExercises={targetMuscleExercises} equipmentExercises={equipmentExercises} />
+    </Box>
+  );
 }
 
 export default ExerciseDetail
