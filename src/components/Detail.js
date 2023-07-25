9f6ee95e2e14622878b44ab9122e24db198c104c
hardikbhasin <hardikbhasin398@gmail.com>
2023-07-25 23:57:19 +0530

    fixed error of detail component in next page

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -5,10 +5,24 @@ import TargetImage from '../assets/icons/target.png';
 import EquipmentImage from '../assets/icons/equipment.png';
 const Detail = ({ exerciseDetail }) => {
   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
+  const extraDetail = [
+    {
+      icon: BodyPartImage,
+      name: bodyPart,
+    },
+    {
+      icon: TargetImage,
+      name: target,
+    },
+    {
+      icon: EquipmentImage,
+      name: equipment,
+    },
+  ];
   return (
     <Stack gap="60px" sx={{ flexDirection: { lg: 'row' }, p: '20px', alignItems: 'center' }}>
       <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
-      {/* <Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
+      {<Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
         <Typography sx={{ fontSize: { lg: '64px', xs: '30px' } }} fontWeight={700} textTransform="capitalize">
           {name}
         </Typography>
@@ -28,7 +42,7 @@ const Detail = ({ exerciseDetail }) => {
             </Typography>
           </Stack>
         ))}
-      </Stack> */}
+      </Stack>}
     </Stack>
   );
 }
