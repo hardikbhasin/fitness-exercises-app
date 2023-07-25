99b5501bbc256150180a11841c60928cae8a3294
hardikbhasin <hardikbhasin398@gmail.com>
2023-07-25 23:59:28 +0530

    added youtube api to fetch and the exerciseVideos

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -9,6 +9,13 @@ export const exerciseOptions = {
         
     },
 };
+export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'd8de15af0fmshd0fc820092f57adp181ecejsn28f8c61cf742',
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+    }
+  };
 export const fetchData = async (url, options) => {
     const response = await fetch(url, options);
     const data = await response.json();
